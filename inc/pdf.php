ef920352027f81802a5e0e50dd89f9aea09c91a0
Pedro Castelo Branco dos Santos <113044972+CastleWhite23@users.noreply.github.com>
2024-05-14 10:52:59 -0300

    Update pdf.php

--- inc/pdf.php
+++ inc/pdf.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require("fpdf.php");
 
 class PDF extends FPDF
@@ -6,14 +6,107 @@ class PDF extends FPDF
     private $titulo;
 
     // Método para definir o título
-    function setTitulo($titulo) {
+    function setTitulo($titulo)
+    {
         $this->titulo = $titulo;
     }
 
+    // Load data
+    function LoadData($file)
+    {
+        // Read file lines
+        $lines = $file;
+        $data = array();
+
+     
+   
+        foreach ($lines as $line)
+        var_dump($line);
+            $new_line= array($line['id'], $line['nome'], $line['user'], $line['foto']);
+            var_dump($new_line);
+    
+            $data[] = explode(';', $new_line);
+       
+        return $data;
+    }
+
+    // Simple table
+    function BasicTable($header, $data)
+    {
+        // Header
+        foreach ($header as $col)
+            $this->Cell(40, 7, $col, 1);
+        $this->Ln();
+        // Data
+        foreach ($data as $row) {
+            
+            foreach ($row as $col)
+            var_dump(!$row['password']);
+                if (!$row['password']) {
+                    $this->Cell(40, 10, $col, 1);
+                }
+    
+            $this->Ln();
+        }
+    }
+
+    // Better table
+    function ImprovedTable($header, $data)
+    {
+        // Column widths
+        $w = array(40, 35, 40, 45);
+        // Header
+        for ($i = 0; $i < count($header); $i++)
+            $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C');
+        $this->Ln();
+        // Data
+        foreach ($data as $row) {
+            $this->Cell($w[0], 6, $row[0], 'LR');
+            $this->Cell($w[1], 6, $row[1], 'LR');
+            $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'R');
+            $this->Cell($w[3], 6, number_format($row[3]), 'LR', 0, 'R');
+            $this->Ln();
+        }
+        // Closing line
+        $this->Cell(array_sum($w), 0, '', 'T');
+    }
+
+    // Colored table
+    function FancyTable($header, $data)
+    {
+        // Colors, line width and bold font
+        $this->SetFillColor(255, 0, 0);
+        $this->SetTextColor(255);
+        $this->SetDrawColor(128, 0, 0);
+        $this->SetLineWidth(.3);
+        $this->SetFont('', 'B');
+        // Header
+        $w = array(40, 35, 40, 45);
+        for ($i = 0; $i < count($header); $i++)
+            $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', true);
+        $this->Ln();
+        // Color and font restoration
+        $this->SetFillColor(224, 235, 255);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        // Data
+        $fill = false;
+        foreach ($data as $row) {
+            $this->Cell($w[0], 6, $row[0], 'LR', 0, 'L', $fill);
+            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
+            $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'R', $fill);
+            $this->Cell($w[3], 6, number_format($row[3]), 'LR', 0, 'R', $fill);
+            $this->Ln();
+            $fill = !$fill;
+        }
+        // Closing line
+        $this->Cell(array_sum($w), 0, '', 'T');
+    }
     //Header
-    function Header(){
-        $this->SetFont("Arial","B",15);
-        $this->Cell(180,10, $this->titulo, 0, 1, "C");
+    function Header()
+    {
+        $this->SetFont("Arial", "B", 15);
+        $this->Cell(180, 10, $this->titulo, 0, 1, "C");
         $this->Ln(20);
     }
 
@@ -21,9 +114,7 @@ class PDF extends FPDF
     function Footer()
     {
         $this->SetY(-15);
-        $this->SetFont("Arial","I",0);
-        $this->Cell(0,10,'Pagina ' . $this->PageNo().  'de (nb)',0,0,'C');
+        $this->SetFont("Arial", "I", 0);
+        $this->Cell(0, 10, 'Pagina ' . $this->PageNo() .  'de (nb)', 0, 0, 'C');
     }
 }
-
-?>
